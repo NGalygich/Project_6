80e0161237e4d43f5d49be6a7d67a6e748ee2e0a
NGalygich <NGalygich@yandex.ru>
2022-05-17 23:08:40 +0300

    First commit

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Project_6;
+
+class Program{
+
+    static public void InputInFile(){
+        short id;
+        string fullName;
+        byte age;
+        sbyte height;
+        string birthDay;
+        string birthPlace;
+    }
+    static public void OutputFromFile(){
+
+    }
+    static public void Main(){
+        Console.WriteLine("1 - Display information about employees;");
+        Console.WriteLine("2 - Add a new entry to the end of the file.");
+        Console.Write("Select action: ");
+        byte choice = Convert.ToByte(Console.ReadLine());
+        if (choice == 1) InputInFile();
+        else OutputFromFile();
+    }
+}
